dc55dfdf18c4df40fc549c81f0b258ff60cd4309
Karen Javadyan <kajarenc@gmail.com>
2022-10-19 18:44:56 +0400

    add custom config

--- app.py
+++ app.py
@@ -7,3 +7,6 @@ st.write("hello world!")
 x = st.slider("X:", 0, 100, 50)
 
 st.write(x**2)
+
+st.write(st.config.get_option("server.enableCORS"))
+st.write(st.config.get_option("server.enableXsrfProtection"))
